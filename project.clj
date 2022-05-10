80412760fd4181204f9f54d478f5448bbfa01fcf
Fabio Domingues <contato@fabiodomingues.com>
2022-05-09 22:46:41 -0300

    Changing to use clj-depend/api

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject com.fabiodomingues/lein-clj-depend "0.1.0"
+(defproject com.fabiodomingues/lein-clj-depend "0.2.0"
   :description "Lein plugin to run clj-depend"
   :url "https://github.com/clj-depend/lein-clj-depend"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true
-  :dependencies [[com.fabiodomingues/clj-depend "0.3.0"]])
+  :dependencies [[com.fabiodomingues/clj-depend "0.4.0"]])
